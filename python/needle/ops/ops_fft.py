8037e8f97b8a658d03c83950f4974c1bbb6b2881
吴柏霖 <bolinw@s-macbook-pro-2.wifi.local.cmu.edu>
2024-11-25 17:45:28 -0500

    fixed ConvFFT pad

--- python/needle/ops/ops_fft.py
+++ python/needle/ops/ops_fft.py
@@ -72,19 +72,16 @@ class ConvFFT(TensorOp):
         self.padding = padding
 
     def compute(self, A, B):
-
+        A = A.pad(((0, 0), (self.padding, self.padding), (self.padding, self.padding), (0, 0)))
         N, H, W, C_in = A.shape
         K, _, _, C_out = B.shape
 
-        padded_h = H + 2 * self.padding
-        padded_w = W + 2 * self.padding
-        A_padded = self._pad_tensor(A, self.padding, self.padding)
+        fft_h = H + K - 1
+        fft_w = W + K - 1
 
-        padded_kh = padded_h + K - 1
-        padded_kw = padded_w + K - 1
-        B_padded = self._pad_tensor(B, padded_kh - K, padded_kw - K)
+        B_padded = B.pad(((0, fft_h - K), (0, fft_w - K), (0, 0), (0, 0)))
 
-        A_fft = fft(A_padded)
+        A_fft = fft(A)
         B_fft = fft(B_padded)
 
         output_fft = A_fft * B_fft
@@ -92,7 +89,7 @@ class ConvFFT(TensorOp):
         output_spatial = ifft(output_fft)
 
         Ho, Wo = (H - K + 2 * self.padding) // self.stride + 1, (W - K + 2 * self.padding) // self.stride + 1
-        output_cropped = output_spatial[:, :, :Ho, :Wo]
+        output_cropped = output_spatial[:, :Ho * self.stride:self.stride, :Wo * self.stride:self.stride, :]
 
         return output_cropped
 
@@ -112,20 +109,6 @@ class ConvFFT(TensorOp):
         B_grad = transpose(transpose(B_grad, (0, 1)), (1, 2))
 
         return A_grad, B_grad
-
-    def _pad_tensor(self, x: Tensor, pad_h: int, pad_w: int) -> Tensor:
-
-        batch_size, channels, height, width = x.shape
-
-        top_pad = init.zeros(batch_size, channels, pad_h, width, device=x.device, dtype=x.dtype)
-        bottom_pad = init.zeros(batch_size, channels, pad_h, width, device=x.device, dtype=x.dtype)
-        left_pad = init.zeros(batch_size, channels, height + 2 * pad_h, pad_w, device=x.device, dtype=x.dtype)
-        right_pad = init.zeros(batch_size, channels, height + 2 * pad_h, pad_w, device=x.device, dtype=x.dtype)
-
-        x_padded = ops.stack([top_pad, x, bottom_pad], axis=2)
-        x_padded = ops.stack([left_pad, x_padded, right_pad], axis=3)
-
-        return x_padded
 
 def conv_fft(a, b, stride=1, padding=1):
     return ConvFFT(stride, padding)(a, b)
